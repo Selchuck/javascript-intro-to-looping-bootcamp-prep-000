d15d9ae94f794a3807626fea0ab8342806dccd76
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-25 11:44:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,15 +1,13 @@
 
-function forloop(array){
-  for(let i = 0; i < 25; i++){
-  if (i === 0) {
-  console.log(`I am 1 ${i} strange loop`);
-    }
-  else {
-  console.log(`I am ${i} strange loops`);
-  }
-  }
-}
 
 function forloop(array){
-  for(let i = 0; i < 25; i++)
+  for(let i = 0; i < 25; i++) {
+    if (i==1){
+      console.log(`I am 1 ${i} strange loop`);
+      else {
+        console.log(`I am ${i} strange loop`);
+      }
+    }
+  }
+  return array;
 }
